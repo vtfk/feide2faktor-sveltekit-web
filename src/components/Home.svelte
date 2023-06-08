7859ae0b38f6a64f8cbaa3982f0a1e3c303a903e
Robin Ellingsen <robin-elli@hotmail.com>
2023-06-08 14:51:18 +0200

    litt tekst

---
+++ src/components/Home.svelte
@@ -0,0 +1,41 @@
+<main>
+    <div class="text">
+        <p>Dette er en selvbetjeningsportal for å betjene din feide 2-faktor satt på din feide konto.</p>
+        <p>Selvbetjeningsportalen kan brukes til å fjerne eksisterende 2-faktor, eller sette en ny 2-faktor til din feide konto.</p>
+        <p>For å benytte deg av selvbetjeningsportalen forutsetter det at du for det første har en feide konto. </p>
+        <p>Du må også ha tilgang til ID-Porten. ID-porten blir brukt for å verifisere at den rette personen logger inn. Du vil få 4 innloggings muligheter:</p>
+        <ol>
+            <li>MINID</li>
+            <li>BANKID</li>
+            <li>BUYPASS ID</li>
+            <li>COMMFIDES</li>
+        </ol>
+        <p>Du står fritt til å velge en av disse 4</p>
+        <p>Det er også viktig å merke seg at det er kun DU som kan sette feide 2-faktor på din konto, det vil si at du som er logget inn nå ikke kan sette feide 2-faktor på en annen person.</p>
+    </div>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+        background-color: var(--hummingBird);
+        padding: 2rem;
+    }
+
+    p {
+        font-size: large;
+        font-weight: 400;
+        padding: 0.2rem;
+    }
+
+    ol {
+        padding-left: 2.5rem;
+    }
+
+    li {
+        font-size: large;
+        font-weight: 400;
+    }
+</style>
--- src/routes/authenticated/+page.svelte
+++ src/routes/authenticated/+page.svelte
@@ -1,12 +1,9 @@
 <script>
-  import { page } from "$app/stores"
-    import Falsly2Faktor from "../../components/Falsly2Faktor.svelte";
-    import FeideUser from "../../components/FeideUser.svelte";
-    import Got2Faktor from "../../components/Got2Faktor.svelte";
-    import NoFeideUser from "../../components/NoFeideUser.svelte";
-  import VerifySecret from "../../components/VerifySecret.svelte";
-  
+    import Home from "../../components/Home.svelte";
 </script>
-  
-<h1>Velkommen</h1>
-<p>Her kommer det noe info</p>
+
+<main>
+  <h1>Velkommen</h1>
+  <br>
+  <Home />
+</main>
