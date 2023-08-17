dc888c28dfd1fd0a1c074905308d89b8221a4c7e
mn3m <matthias.krauss@gmail.com>
2023-08-17 12:54:43 +0200

    Litt tekst- og layoutfiks

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,17 +1,18 @@
 <main>
     <div class="text">
-        <p>Dette er en selvbetjeningsportal for å betjene din feide 2-faktor satt på din feide konto.</p>
-        <p>Selvbetjeningsportalen kan brukes til å fjerne eksisterende 2-faktor, eller sette en ny 2-faktor til din feide konto.</p>
-        <p>For å benytte deg av selvbetjeningsportalen forutsetter det at du for det første har en feide konto. </p>
-        <p>Du må også ha tilgang til ID-Porten. ID-porten blir brukt for å verifisere at den rette personen logger inn. Du vil få 4 innloggings muligheter:</p>
+        <p>Dette er en selvbetjeningsportal for å betjene 2-faktor satt på din feidekonto.</p>
+        <p>Selvbetjeningsportalen kan brukes til å fjerne eksisterende 2-faktor eller sette en ny 2-faktor til din feidekonto.</p>
+        <p>For å benytte deg av selvbetjeningsportalen forutsetter det at du har en feidekonto. </p>
+        <p>Du må også ha tilgang til ID-porten. ID-porten blir brukt for å verifisere at den rette personen logger inn. Du vil få 4 innloggingsmuligheter og står fritt til å velge en av disse:</p>
         <ol>
             <li>MINID</li>
             <li>BANKID</li>
             <li>BUYPASS ID</li>
             <li>COMMFIDES</li>
         </ol>
-        <p>Du står fritt til å velge en av disse 4</p>
         <p>Det er også viktig å merke seg at det er kun DU som kan sette feide 2-faktor på din konto, det vil si at du som er logget inn nå ikke kan sette feide 2-faktor på en annen person.</p>
+        <p></p>
+        <p>Klikk på "Feide" i menyen til venstre for å administrere 2-faktor.</p>
     </div>
 </main>
 
@@ -31,7 +32,7 @@
     }
 
     ol {
-        padding-left: 2.5rem;
+        padding: 1rem 0rem 1.5rem 2.5rem
     }
 
     li {
